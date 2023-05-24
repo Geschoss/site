e2734048c574ad56737ba1d8f9d45ed07e9b4828
Pavel Kolomnikov <q11254@gmail.com>
2023-05-25 00:40:32 +0300

    roll back http

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,12 @@
-let https = require('https');
-let fs = require('fs');
+let http = require('http');
 let { requestListener } = require('./listener.js');
 let { makeRoute } = require('./route.js');
 
 const server = {
     start({ port, logger, env }) {
         const route = makeRoute({ logger, env });
-        const httpsOptions = {
-            key: fs.readFileSync(`${env.paths.security}/key.pem`),
-            cert: fs.readFileSync(`${env.paths.security}/cert.pem`),
-        };
-
-        https
-            .createServer(httpsOptions, requestListener({ logger, env, route }))
+        http
+            .createServer(requestListener({ logger, env, route }))
             .listen(port, () => {
                 console.log(`Server start on port: ${port}`);
             });
