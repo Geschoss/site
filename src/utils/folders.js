522ca851e11b7c1591e1f754657382b0c0c65bb5
Valentin Schegolev <maracas20011@gmail.com>
2023-06-14 15:58:41 +0300

    removing extra lines

--- src/utils/folders.js
+++ src/utils/folders.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 function getChildrenFrom(src) {
-    return (promise = new Promise(function (resovle) {
+    return new Promise(function (resovle) {
         fs.readdir(src, (err, files) => {
             let arr = [];
             let count = 0;
@@ -14,10 +14,8 @@ function getChildrenFrom(src) {
                 }
             });
         });
-    }));
+    });
 }
-
-const gameResolvedPath = path.resolve('src/games');
 
 getChildrenFrom(gameResolvedPath).then((arr) => {
     return arr;
